dead7984e7e633be24d32cdb6fc45e7985357376
ht gong <gonghaotong@outlook.com>
2023-12-27 12:43:52 +0800

    完成用户对战记录查询的前后端实现，尚未完成分页功能和对战回放功能

--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -1,19 +1,87 @@
 <template>
     <ContentField>
-        对局列表
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>A</th>
+                    <th>B</th>
+                    <th>对战结果</th>
+                    <th>对战时间</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="record in records" :key="record.record.id">
+                    <td>
+                        <img :src="record.a_photo" alt="" class="record-user-photo">
+                        &nbsp;
+                        <span class="record-user-username">{{ record.a_username }}</span>
+                    </td>
+                    <td>
+                        <img :src="record.b_photo" alt="" class="record-user-photo">
+                        &nbsp;
+                        <span class="record-user-username">{{ record.b_username }}</span>
+                    </td>
+                    <td>{{ record.result }}</td>
+                    <td>{{ record.record.createtime }}</td>
+                    <td>
+                        <button type="button" class="btn btn-primary">查看录像</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </ContentField>
 </template>
 
 <script>
 import ContentField from '../../components/ContentField.vue'
+import { useStore } from 'vuex';
+import { ref } from 'vue';
+import $ from 'jquery';
 
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        let records = ref([]);
+        let current_page = 1;
+        let total_records = 0;
+
+        const pull_page = page => {
+            current_page = page;
+            $.ajax({
+                url: "http://127.0.0.1:3000/record/getlist/",
+                data: {
+                    page,
+                },
+                type: "get",
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    records.value = resp.records;
+                    total_records.value = resp.record_count;
+                },
+                error(resp) {
+                    console.log(resp);
+                }
+            })
+        }
+
+        pull_page(current_page);  
+
+        return {
+            records,
+        }
     }
 }
 </script>
 
 <style scoped>
-
+img.record-user-photo {
+    width: 6vh;
+    border-radius: 70%;
+}
 </style>
