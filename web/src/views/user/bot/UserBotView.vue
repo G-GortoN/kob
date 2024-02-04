4fa765bb338a335cabd8b9bec7656273b74cf7f2
ht gong <gonghaotong@outlook.com>
2024-02-04 20:30:34 +0800

    迁移到github

--- web/src/views/user/bot/UserBotView.vue
+++ web/src/views/user/bot/UserBotView.vue
@@ -154,7 +154,7 @@ export default {
 
         const refresh_bots = () => {
             $.ajax({
-                url: "http://127.0.0.1:3000/user/bot/getlist/",
+                url: "https://app6506.acapp.acwing.com.cn/api/user/bot/getlist/",
                 type: "get",
                 headers: {
                     Authorization: "Bearer " + store.state.user.token,
@@ -170,7 +170,7 @@ export default {
         const add_bot = () => {
             bot_add.error_message = "";
             $.ajax({
-                url: "http://127.0.0.1:3000/user/bot/add/",
+                url: "https://app6506.acapp.acwing.com.cn/api/user/bot/add/",
                 type: "post",
                 data: {
                     title: bot_add.title,
@@ -194,7 +194,7 @@ export default {
         const update_bot = (bot) => {
             bot_add.error_message = "";
             $.ajax({
-                url: "http://127.0.0.1:3000/user/bot/update/",
+                url: "https://app6506.acapp.acwing.com.cn/api/user/bot/update/",
                 type: "post",
                 data: {
                     bot_id: bot.id,
@@ -221,7 +221,7 @@ export default {
 
         const remove_bot = (bot) => {
             $.ajax({
-                url: "http://127.0.0.1:3000/user/bot/remove/",
+                url: "https://app6506.acapp.acwing.com.cn/api/user/bot/remove/",
                 type: "post",
                 data: {
                     bot_id: bot.id,
